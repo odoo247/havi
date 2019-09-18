bbfc9f84fd348cece4db466f9c702efba53591f6
root <minhtoansgu@gmail.com>
2019-09-18 10:18:36 +0700

    commit

--- hv_project/models/project.py
+++ hv_project/models/project.py
@@ -10,6 +10,32 @@ class ProjectExtend(models.Model):
     hour_support=fields.Float("Hours Support")
     remaining_hour=fields.Float("Remaining",readonly=True)
     percent=fields.Float("Percent Reminder")
+    def send_email(self):
+        template_id=self.env.ref('hv_project.email_template_project_project_remind_first').id
+        composer=self.env['mail.compose.message'].with_context({
+            'default_composition_mode':'comment',
+            'default_model':'project.project',
+            'default_res_id':self.id,
+            'default_template_id':template_id
+        }).create({'subject':'abc','body':'hello'})
+
+        values=composer.onchange_template_id(template_id,'comment',self._name,self.id)['value']
+        composer.write(values)
+        composer.send_mail()
+
+    def send_email_second(self):
+        template_id=self.env.ref('hv_project.email_template_project_project_remind_second').id
+        composer=self.env['mail.compose.message'].with_context({
+            'default_composition_mode':'comment',
+            'default_model':'project.project',
+            'default_res_id':self.id,
+            'default_template_id':template_id
+        }).create({'subject':'abc','body':'hello'})
+
+        values=composer.onchange_template_id(template_id,'comment',self._name,self.id)['value']
+        composer.write(values)
+        composer.send_mail()
+
 
     @api.onchange('hour_support')
     def change_value(self):
@@ -26,7 +52,6 @@ class TaskExtend(models.Model):
     _inherit="project.task"
 
     remain_hour_support=fields.Float("Remain Hours Support",compute='_computer_hour_support',readonly=True)
-
     def _computer_hour_support(self):
         total_hours_project=self.project_id.hour_support
         total_tasks=self.env['project.task'].search([('project_id','=',self.project_id.name)])
@@ -40,22 +65,12 @@ class TaskExtend(models.Model):
             if total_hours_project != 0:
                 if not state:
                     if (total_hours_project/self.project_id.hour_support)*100 < self.project_id.percent:
-                        template_id = self.env.ref('hv_project.email_template_project_task_remind_first').id
-                        template = self.env['mail.template'].browse(template_id)
-                        follower = self.env['project.project'].search([('name', '=', self.project_id.name)]).message_follower_ids
-                        for i in follower:
-                            template.write({'email_to': i.partner_id.email})
-                            template.send_mail(self.id, force_send=True)
-                    this_project.write({'first_reminder': True})
+                        self.env['project.project'].search([('name','=',self.project_id.name)]).send_email()
+                        this_project.write({'first_reminder': True})
             second_reminder=self.env['project.project'].search([('name','=',self.project_id.name)])
             if not second_reminder.second_reminder:
                 if total_hours_project <= 0:
-                    template_id = self.env.ref('hv_project.email_template_project_task_remind_second').id
-                    template = self.env['mail.template'].browse(template_id)
-                    follower = self.env['project.project'].search([('name', '=', self.project_id.name)]).message_follower_ids
-                    for i in follower:
-                        template.write({'email_to': i.partner_id.email})
-                        template.send_mail(self.id, force_send=True)
+                    self.env['project.project'].search([('name','=',self.project_id.name)]).send_email_second()
                     second_reminder.write({'second_reminder':True})
         self.remain_hour_support=total_hours_project
         this_project.write({'remaining_hour':total_hours_project})
